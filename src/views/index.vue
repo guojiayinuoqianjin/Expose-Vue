cca32d174b4ba2c9b7fe7d930adfd8a2ff949986
jeremy16601 <lixueqing1127@gmail.com>
2017-08-30 15:13:52 +0800

    修改首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,58 +1,63 @@
 <style scoped>
-    .index {
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        text-align: center;
-    }
+.index {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    text-align: center;
+}
 
-    .index h1 {
-        height: 150px;
-    }
+.index h1 {
+    height: 50px;
+}
 
-    .index h1 img {
-        height: 100%;
-    }
+.index h1 img {
+    height: 100%;
+}
 
-    .index h2 {
-        color: #666;
-        margin-bottom: 200px;
-    }
+.index h2 {
+    color: #666;
+    margin-bottom: 200px;
+}
 
-    .index h2 p {
-        margin: 0 0 50px;
-    }
+.index h2 p {
+    margin: 20px 0 50px;
+}
 
-    .index .ivu-row-flex {
-        height: 100%;
-    }
+.index .ivu-row-flex {
+    height: 100%;
+}
+img{
+    width: 120px;
+    height: 120px;
+}
 </style>
 <template>
     <div class="index">
         <Row type="flex" justify="center" align="middle">
             <Col span="24">
-                <h1>
-                   曝光平台
-                </h1>
-                <h2>
-                    <p>Welcome to your 曝光平台!</p>
-                    <Button type="ghost" @click="handleStart">Start </Button>
-                </h2>
+            <h1>
+                曝光平台
+            </h1>
+            <div class="demo-avatar">
+               <img src="http://api.bbaaoo.cn/public/image/bao.png" alt="">
+            </div>
+
+            <h2>
+                <p>曝光吧，让更多人防止被骗！</p>
+                <Button type="ghost" @click="handleStart">下载app </Button>
+            </h2>
             </Col>
         </Row>
     </div>
 </template>
 <script>
-    export default {
-        methods: {
-            handleStart() {
-                this.$Modal.info({
-                    title: '提示',
-                    content: '曝光平台即将上线，请耐心等待.'
-                });
-            }
+export default {
+    methods: {
+        handleStart() {
+            window.location.href = "http://api.bbaaoo.cn/public/download.html";
         }
-    };
+    }
+};
 </script>
